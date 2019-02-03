5bda5d80bf30387bbcefff6e08be5c5c83246a3c
Maxwell Gurewitz <maxthegeek1@gmail.com>
2019-02-03 10:54:54 -0800

    empty plugin

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function helloWorld() {
-    console.log("hello world");
-}
-exports["default"] = helloWorld;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-export default function helloWorld() {
-  console.log("hello world");
+import Hapi from 'hapi';
+import pkg from '../package.json';
+
+interface PluginOptions {
+
 }
+
+const plugin : Hapi.Plugin<PluginOptions> = {
+  pkg,
+  async register(server : Hapi.Server, options : PluginOptions) {
+  }
+};
+
+export default plugin;
